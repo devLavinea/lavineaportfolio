110bad9988739a433a82a6a758a1acd8ff054b13
devLavinea <lavinea2411@gmail.com>
2025-04-05 20:05:50 -0300

    bug

---
+++ src/components/index.js
@@ -0,0 +1,36 @@
+import { 
+    EarthCanvas,
+    BallCanvas,
+    ComputersCanvas,
+    StarsCanvas,
+    CanvasLoader,
+ } from "../canvas";
+
+import Whatsapp from "./Whatsapp";
+import Navbar from "./Navbar";
+import Cta from "./Cta";
+import Hero from "./Hero";
+import Services from "./Services";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+
+export {
+  Whatsapp,
+  Navbar,
+  Cta,
+  Hero,
+  Services,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+    EarthCanvas,
+    BallCanvas,
+    ComputersCanvas,
+    StarsCanvas,
+    CanvasLoader,
+};
