b3e1b8f5f02d5963e4c1a6719d1d501f2461fb62
devlavinea <lavinea2411@gmail.com>
2025-04-06 19:14:04 -0300

    feat: whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import { FaWhatsapp } from "react-icons/fa";
 
 import Cta from "./components/Cta";
 import Hero from "./components/Hero";
@@ -34,6 +35,14 @@ const App = () => {
         <Contact />
         <Stars/>
       </div>
+      <div className="whatsapp w-[60px] h-[60px] md:w-[75px] md:h-[75px] lg:w-[70px] lg:h-[70px] ">
+      <a
+        href="https://wa.me/5574988248014?text=Olá,%20Lavinea,%20vi%20seu%20portfólio%20e%20tenho%20interesse%20em%20obter%20mais%20informações."
+        target="_blank"
+      >
+        <FaWhatsapp />
+      </a>
+    </div>
     </div>
   );
 };
