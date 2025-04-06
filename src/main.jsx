ef7d3089044498aa74506c4c4a4cdd5d452391ce
devLavinea <lavinea2411@gmail.com>
2025-04-05 22:18:16 -0300

    bug

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Project from "./components/Modal";
+// import Project from "./components/Modal";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/:id" element={<Project />} />
+        {/* <Route path="/:id" element={<Project />} /> */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
